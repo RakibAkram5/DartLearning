c3f958de489fef197ca53d42f13ef43c5a7a5143
Muhammad Rakib Akram <rakibakram55@gmail.com>
2026-05-07 11:00:26 +0500

    initial commit

---
+++ Swapping.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 20;
+  int b = 10;
+  int temp;
+  print("Before Swapping a:$a and b: $b");
+  temp = a;
+  a = b;
+  b = temp;
+  print("After swapping a:$a and b:$b");
+}
